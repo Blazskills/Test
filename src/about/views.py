72f8b80e75eab5f1070a0abf4e8ab70c951c6989
Blazskills <ilesanmiisaac@gmail.com>
2020-01-07 00:06:16 +0100

    added changes to portfolio view and index page

--- src/about/views.py
+++ src/about/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from .models import Owner
+# from django.shortcuts import render
+# from .models import Owner
 
-def index(request):
-    queryset = Owner.objects.all()
-    context = {
-          'object_list': queryset,
-     }
-    print (context)
-    return render(request, 'index.html', context )
+# def index(request):
+#     queryset = Owner.objects.all()
+#     context = {
+#           'object_list': queryset,
+#      }
+#     print (context)
+#     return render(request, 'index.html', context )
 
